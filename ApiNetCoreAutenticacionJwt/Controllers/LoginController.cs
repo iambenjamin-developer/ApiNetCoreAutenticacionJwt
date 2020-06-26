7a2b7bb61ea4af4c9486c5fd64442dad830cf9ef
Your Name <you@example.com>
2020-06-26 01:12:48 -0300

    first commit

---
+++ ApiNetCoreAutenticacionJwt/Controllers/LoginController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ApiNetCoreAutenticacionJwt.Models;
+using ApiNetCoreAutenticacionJwt.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiNetCoreAutenticacionJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<UserToken>> ValidarLogin([FromBody] LoginUser loginUser)
+        {
+            var servicio = new ServicioOracle();
+            bool validacionOK = servicio.ValidarLogin(loginUser);
+            List<string> roles = new List<string>() { "admin", "comercial", "personal" };
+
+
+            if (validacionOK == true)
+            {
+                return BuildToken(loginUser, roles);
+            }
+            else
+            {
+
+                ModelState.AddModelError("Usuario/Clave", "Usuario o clave incorrectos");
+                return BadRequest(ModelState);
+            }
+
+        }
+
+
+        private UserToken BuildToken(LoginUser loginUser, IList<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+        new Claim(JwtRegisteredClaimNames.UniqueName, loginUser.Usuario),
+        new Claim(JwtRegisteredClaimNames.Email, loginUser.Usuario + "@cyre.com.ar"),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+            foreach (var rol in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, rol));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Tiempo de expiración del token. En nuestro caso lo hacemos de media hora
+            var expiration = DateTime.UtcNow.AddMinutes(30);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+               issuer: null,
+               audience: null,
+               claims: claims,
+               expires: expiration,
+               signingCredentials: creds);
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+
+    }
+}
---
+++ ApiNetCoreAutenticacionJwt/Services/ServicioOracle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.Linq;
+using System.Text;
+using ApiNetCoreAutenticacionJwt.Models;
+using Dapper;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ApiNetCoreAutenticacionJwt.Services
+{
+    public class ServicioOracle
+    {
+        public bool ValidarLogin(LoginUser loginUser)
+        {
+            string conexionString = @"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=XE)));User Id=CYRE;Password=123456;";
+
+            StringBuilder sqlQuery = new StringBuilder();
+            sqlQuery.Append("SELECT COUNT(*) ");
+            sqlQuery.Append("FROM LOGIN ");
+            sqlQuery.Append("WHERE USUARIO = :usuarioParametro ");
+            sqlQuery.Append("AND CLAVE = :claveParametro ");
+
+
+
+            int entero = 0;
+            using (var connection = new OracleConnection(conexionString))
+            {
+                var parameters = new { usuarioParametro = loginUser.Usuario, claveParametro = loginUser.Clave };
+
+                entero = connection.ExecuteScalar<int>(sqlQuery.ToString(), parameters);
+            }
+
+            if (entero == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
